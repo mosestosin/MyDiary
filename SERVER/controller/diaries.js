3d1f2bd041f6f1911addab2bc05cc8624155f1a5
mosestosin <mosestosin35@gmail.com>
2018-07-22 23:45:49 +0100

    Add The update-diary-entry branch

--- SERVER/controller/diaries.js
+++ SERVER/controller/diaries.js
@@ -5,49 +5,49 @@ class Diary {
     return res.status(200).json({ diaries });
   }
 
-//   static getSpecificDiary(req, res) {
-//     diaries.forEach((diary) => {
-//       if (diary.id === parseInt(req.params.id, 10)) {
-//         res.status(200).json({ diary });
-//       }
-//     });
-//     return res.status(404).json({
-//       message: 'diary not found',
-//       error: true,
-//     });
-//   }
+  static getSpecificDiary(req, res) {
+    diaries.forEach((diary) => {
+      if (diary.id === parseInt(req.params.id, 10)) {
+        res.status(200).json({ diary });
+      }
+    });
+    return res.status(404).json({
+      message: 'diary not found',
+      error: true,
+    });
+  }
 
-//   static createDiary(req, res) {
-//     diaries.push({
-//       id: diaries.length + 1,
-//       title: req.body.title,
-//       content: req.body.content,
-//       createdAt: new Date(),
-//     });
-//     res.status(200).json({
-//       diaries,
-//       message: 'success',
-//       error: false,
-//     });
-//   }
+  static createDiary(req, res) {
+    diaries.push({
+      id: diaries.length + 1,
+      title: req.body.title,
+      content: req.body.content,
+      createdAt: new Date(),
+    });
+    res.status(200).json({
+      diaries,
+      message: 'success',
+      error: false,
+    });
+  }
 
-//   static updateDiary(req, res) {
-//     diaries.forEach((diary) => {
-//       if (diary.id === parseInt(req.params.id, 10)) {
-//         diary.title = req.body.title;
-//         diary.content = req.body.content;
-//         res.status(200).json({
-//           diary,
-//           message: 'success',
-//           error: false,
-//         });
-//       }
-//     });
-//     return res.status(404).json({
-//       message: 'diary not found',
-//       error: true,
-//     });
-//   }
+  static updateDiary(req, res) {
+    diaries.forEach((diary) => {
+      if (diary.id === parseInt(req.params.id, 10)) {
+        diary.title = req.body.title;
+        diary.content = req.body.content;
+        res.status(200).json({
+          diary,
+          message: 'success',
+          error: false,
+        });
+      }
+    });
+    return res.status(404).json({
+      message: 'diary not found',
+      error: true,
+    });
+  }
 }
 
 export default Diary;
