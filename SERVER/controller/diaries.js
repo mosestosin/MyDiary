2826cce6189bd9c670bc8483776a3b39a464eb2e
mosestosin <mosestosin35@gmail.com>
2018-07-22 21:55:58 +0100

    Add The add-diary-entry branch

--- SERVER/controller/diaries.js
+++ SERVER/controller/diaries.js
@@ -5,31 +5,31 @@ class Diary {
     return res.status(200).json({ diaries });
   }
 
-//   static getSpecificDiary(req, res) {
-//     diaries.forEach((diary) => {
-//       if (diary.id === parseInt(req.params.id, 10)) {
-//         res.status(200).json({ diary });
-//       }
-//     });
-//     return res.status(404).json({
-//       message: 'diary not found',
-//       error: true,
-//     });
-//   }
+  static getSpecificDiary(req, res) {
+    diaries.forEach((diary) => {
+      if (diary.id === parseInt(req.params.id, 10)) {
+        res.status(200).json({ diary });
+      }
+    });
+    return res.status(404).json({
+      message: 'diary not found',
+      error: true,
+    });
+  }
 
-//   static createDiary(req, res) {
-//     diaries.push({
-//       id: diaries.length + 1,
-//       title: req.body.title,
-//       content: req.body.content,
-//       createdAt: new Date(),
-//     });
-//     res.status(200).json({
-//       diaries,
-//       message: 'success',
-//       error: false,
-//     });
-//   }
+  static createDiary(req, res) {
+    diaries.push({
+      id: diaries.length + 1,
+      title: req.body.title,
+      content: req.body.content,
+      createdAt: new Date(),
+    });
+    res.status(200).json({
+      diaries,
+      message: 'success',
+      error: false,
+    });
+  }
 
 //   static updateDiary(req, res) {
 //     diaries.forEach((diary) => {
